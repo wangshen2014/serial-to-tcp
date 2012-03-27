2d27d88b8936da2d3ec7b05375bd1c85346a12fb
Daan Kets <daankets@blackbit.be>
2012-03-27 13:06:38 +0200

    Fixes
    Fixed node (engines) version.
    Fixed start script for global installation.

--- development/src/main/sh/serialserver.sh
+++ development/src/main/sh/serialserver.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
 
 DIR=$(cd $(dirname "$0"); pwd)
-node "$DIR"/../lib/main.js $1 $2 $3 $4
+PREFIX=$(npm prefix)
+GLOBAL_PREFIX=$(npm -g prefix)
+
+CURRENT=$DIR/../lib/main.js
+LOCAL=$PREFIX/lib/main.js
+GLOBAL=$GLOBAL_PREFIX/lib/node_modules/serialserver/lib/main.js
+
+# Use the local copy if installed locally.
+if [ -f "$CURRENT" ];
+then
+	TARGET=$CURRENT;
+else
+	if [ -f "$LOCAL" ];
+	then
+		TARGET=$LOCAL
+	else
+		TARGET=$GLOBAL
+	fi
+fi
+
+node "$TARGET" $1 $2 $3 $4
